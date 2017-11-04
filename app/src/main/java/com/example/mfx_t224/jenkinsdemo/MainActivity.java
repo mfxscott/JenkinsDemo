b3b35b9d4289bd2c6c96532564781cb3c0a20ecc
mfx <mfx19900211@gmail.com>
2017-11-04 10:38:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mfx_t224/jenkinsdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mfx_t224.jenkinsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
